399f040f3248e4723d0fd6ff3d6abe8d3598fd56
Nabeel <muhammadnabeeljavede@gmail.com>
2025-03-25 01:01:51 +0500

    Remove commented error handling for no comments found in currentUserBlogs

--- src/controllers/blog.controller.js
+++ src/controllers/blog.controller.js
@@ -72,9 +72,9 @@ const currentUserBlogs = asyncHandler(async (req, res) => {
         return acc + blog.comments.length
     }, 0)
 
-    if (!totalBlogsComments) {
-        throw new ApiError(404, "No comments found for this user")
-    }
+    // if (!totalBlogsComments) {
+    //     throw new ApiError(404, "No comments found for this user")
+    // }
 
 
     return res.status(200).json(new ApiResponse(200, "User blogs", { userBlogs, totalBlogsComments }));
